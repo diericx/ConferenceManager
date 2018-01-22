3dbc3a694401da6384b3064c169bb5f8ff65a3ed
Zac Holland <zacharyholland@gmail.com>
2018-01-22 12:24:17 -0700

    removed final review form reviws list

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -37,9 +37,8 @@ class SubmissionsController < ApplicationController
     
     # Users assigned to review this report
     @reviewers = ReviewerAssignment.where(submission_id: @submission.id)
-    @submission_reviews = SubmissionReview.where(submission_id: @submission.id)
+    @submission_reviews = SubmissionReview.where(submission_id: @submission.id, final: false)
     @submission_reviews.each do |review|
-      puts review.reviewer_email
       review.reviewer_email = User.find(review.reviewer_id).name
     end
 
